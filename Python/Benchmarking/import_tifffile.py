9a197534cec05e52174750739d73b3ea01efc01a
Dominik Sagebiel <d.sagebiel@campus.tu-berlin.de>
2026-09-08 16:45:05 +0200

    added gitignore

--- Python/Benchmarking/import_tifffile.py
+++
@@ -1,4 +0,0 @@
-import tifffile
-with tifffile.TiffFile(r"C:\Users\domin\Nextcloud\Uni\FU Ba. BioInf\11. Sem\Bachelorarbeit\data\mIF\LymphNode\adjacent\Core_13.ome.tif") as tif:
-    print(tif.series[0].axes, tif.series[0].shape)
-
